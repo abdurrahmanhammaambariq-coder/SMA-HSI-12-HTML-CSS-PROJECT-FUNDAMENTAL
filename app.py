afc05ffcb0dc3ac83a7a4d7f57b7e7c9216f6a0a
abdurrahmanhammaambariq-coder <abdurrahmanhammaambariq>
2025-10-07 10:05:33 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+# # 1. Impor "cetakan" Flask
+# from flask import Flask 
+# # 2. Buat "objek" aplikasi dari cetakan tersebut 
+# app = Flask(__name__) 
+# # 3. Buat "alamat" atau "rute" untuk halaman utama
+# @app.route('/')
+# def halaman_utama(): 
+# # 4. Tentukan apa yang harus ditampilkan di alamat ini 
+#     return "Halo dari Server Flask!" 
+# # 5. (Opsional tapi bagus) Baris untuk menjalankan aplikasi
+# if __name__ == '__main__': 
+#     app.run(debug=True) 
+
+
+
+# latihan pertama
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/profil")
+def halaman_profil():
+    nama_penguguna = "Baba"
+    hobi_pengguna = "bola"
+    sekolah_pengguna = "HSI"
+    return render_template("profil.html", user_name=nama_penguguna,user_hobby=hobi_pengguna,user_school=sekolah_pengguna)
+
+@app.route("/teman")
+def halaman_teman():
+    daftar_teman = ['gog','gug','gag']
+    return render_template("teman.html", daftar_teman=daftar_teman )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
