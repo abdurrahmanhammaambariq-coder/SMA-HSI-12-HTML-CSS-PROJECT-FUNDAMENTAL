53e1dc37009b5bfc067e988adf185fd7dad74e5a
abdurrahmanhammaambariq-coder <abdurrahmanhammaambariq>
2025-10-24 09:49:26 +0700

    first commit

--- app.py
+++ app.py
@@ -17,29 +17,24 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+semua_pesan = []  # database sementara
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/profil")
-def halaman_profil():
-    nama_penguguna = "Baba"
-    hobi_pengguna = "bola"
-    sekolah_pengguna = "HSI"
-    return render_template("profil.html", user_name=nama_penguguna,user_hobby=hobi_pengguna,user_school=sekolah_pengguna)
-
-@app.route('/bukutamu',methods=['GET','POST'])
+@app.route('/bukutamu', methods=['GET', 'POST'])
 def halaman_teman():
-    if request.method == 'GET':
-        return render_template('bukutamu.html')
+    global semua_pesan
+    
     if request.method == 'POST':
         nama = request.form.get('nama')
         pesan = request.form.get('pesan')
 
-    return render_template('bukutamu.html', nama=nama, pesan=pesan)
+        data_baru = {'nama': nama, 'pesan': pesan}
+        semua_pesan.append(data_baru)
 
+    return render_template('bukutamu.html', semua_pesan=semua_pesan)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
